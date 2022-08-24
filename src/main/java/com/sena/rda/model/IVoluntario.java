6687a9ffcd0926a74f1100b70419a05ee60c62a3
Kevin Nicolas Ceron Catumba <knceron4@misena.edu.co>
2022-08-24 08:50:31 -0500

    Prueba prueba

--- src/main/java/com/sena/rda/model/IVoluntario.java
+++
@@ -1,9 +0,0 @@
-package com.sena.rda.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IVoluntario  extends CrudRepository <Voluntario, Integer>{
-    
-}
-    
-
