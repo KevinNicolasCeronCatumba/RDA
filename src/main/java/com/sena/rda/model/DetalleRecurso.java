2ebb94da5518fdb17239b98c71f98d3d1735757e
Kevin Nicolas Ceron Catumba <knceron4@misena.edu.co>
2022-09-04 15:48:39 -0500

    ACTUALIZACION EN TABLA DETALLE RECURSO, DISEÑO DE USUARIO (NICOLAS)

--- src/main/java/com/sena/rda/model/DetalleRecurso.java
+++ src/main/java/com/sena/rda/model/DetalleRecurso.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="detallerecurso")
@@ -23,4 +24,10 @@ public class DetalleRecurso {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Evento evento;
+
+    @NotEmpty
+    private Integer tipoRecurso;
+    
+    @NotEmpty
+    private Integer Cantidad;
 }
